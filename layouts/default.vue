ede8312312dd620f0c7c6ea3190612c34c2f5301
waset <waset@foxmail.com>
2023-02-21 16:47:32 +0800

    feat(layouts): :bricks: 公共布局

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="common-layout">
+        <el-container style="height: 100vh;">
+            <el-header style="background-color: red;">Header</el-header>
+            <el-container>
+                <el-aside width="200px" style="background-color: green;">Aside</el-aside>
+                <el-main>
+                    <slot />
+                </el-main>
+            </el-container>
+        </el-container>
+    </div>
+</template>
+
+<style lang="css" >
+body {
+    padding: 0;
+    margin: 0;
+}
+</style>
