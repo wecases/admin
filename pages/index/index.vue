23557f2b8747de07279442d2bba73d58c8000150
waset <waset@foxmail.com>
2023-02-21 16:04:05 +0800

    build(deps): :package: add Elemelt-plus

---
+++ pages/index/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    console.log(123);
+
+</script>
+
+<template>
+    <div>
+        <h1>Home</h1>
+        <p>Home page</p>
+        <el-button type="primary">Primary</el-button>
+    </div>
+</template>
